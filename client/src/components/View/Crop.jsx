6c802449bbb01f017100404c20e02da5dc3b402f
fatima-batool631 <fatimabatool6.dev@gmail.com>
2025-10-29 16:19:44 +0500

    Updated crop component design

--- client/src/components/View/Crop.jsx
+++ client/src/components/View/Crop.jsx
@@ -1,51 +1,80 @@
 import React from "react";
-import { formatDateYYYYMMDD } from "../../utils/date";
 import "./Crop.css";
 
 const Crop = (props) => {
-  let data = props.crop;
-  let name=props.componentName;
+  const data = props.crop;
+  const name = props.componentName;
 
   if (!data) {
     return <div>Loading crop data...</div>;
   }
 
- 
-    return (
+  const formatShortDate = (dateString) => {
+    const date = new Date(dateString);
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return `${months[date.getMonth()]} ${date.getDate()}`;
+  };
+
+  return (
+    <div className="crop-container">
       <div className="crop-wrapper">
         <div className="container">
-          <h3>{name}</h3>
-          <h2 className="crop-title">
-            {data.icon} {data.name}
-          </h2>
+          <div className="crop-header">
+            <h3 className="comp-name">{name}</h3>
+            <div className="crop-icon-box">
+              <span className="crop-icon">{data.icon}</span>
+            </div>
+          </div>
+          <div className="crop-title-box">
+            <h2 className="crop-title">{data.name}</h2>
+          </div>
 
-          <div className="crop-details">
-            <p>
-              <strong>Seeding Period:</strong>{" "}
-              {formatDateYYYYMMDD(data.seedDateRangeStart)} -{" "}
-              {formatDateYYYYMMDD(data.seedDateRangeEnd)}
-            </p>
-            <p>
-              <strong>Harvest Period:</strong>{" "}
-              {formatDateYYYYMMDD(data.harvestDateRangeStart)} -{" "}
-              {formatDateYYYYMMDD(data.harvestDateRangeEnd)}
-            </p>
-            <p>
-              <strong>Temperature Range:</strong> {data.tempRangeStart}°C -{" "}
-              {data.tempRangeEnd}°C
-            </p>
-            <p>
-              <strong>Humidity Range:</strong> {data.humidRangeStart}% -{" "}
-              {data.humidRangeEnd}%
-            </p>
-            <p>
-              <strong>Expected Yield:</strong> {data.yield}
-            </p>
+          {/* 4 Info Boxes in 2x2 Grid */}
+          <div className="crop-info-grid">
+            <div className="crop-info-box">
+              <div className="crop-info-icon-small">Yield</div>
+              <div className="crop-info-value">{data.yield}</div>
+            </div>
+
+            <div className="crop-info-box">
+              <div className="crop-info-icon-small">Harvest Period</div>
+              <div className="crop-info-label">
+                {formatShortDate(data.harvestDateRangeStart)} -{" "}
+                {formatShortDate(data.harvestDateRangeEnd)}
+              </div>
+            </div>
+
+            <div className="crop-info-box">
+              <div className="crop-info-icon-small">Temperature Range</div>
+              <div className="crop-info-value-inline">
+                {data.tempRangeStart}°C - {data.tempRangeEnd}°C
+              </div>
+            </div>
+
+            <div className="crop-info-box">
+              <div className="crop-info-icon-small">Humidity Range</div>
+              <div className="crop-info-value-inline">
+                {data.humidRangeStart}% - {data.humidRangeEnd}%
+              </div>
+            </div>
           </div>
         </div>
-       </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 
 export default Crop;
