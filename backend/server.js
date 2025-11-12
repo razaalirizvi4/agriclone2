455223e26167ccf7e7946a521753518bcee82e73
Abdullah70142284 <abdullahghouri0211@gmail.com>
2025-11-12 18:12:03 +0500

    Revert "WIP: local changes before pull"
    This reverts commit 67275c94e37b51051e9956c14186bb6b8da3968f.

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const serviceRegistry = require('./services/serviceRegistry');
 const eventStreamService = require('./services/eventStream.service.js');
 const ensureRoles = require('./serverSetup/ensureRoles');
 
-serviceRegistry.register("eventStreamService", eventStreamService);
-const cors = require("cors");
-const connectDB = require("./serverSetup/database");
-require("./services/scheduler");
+serviceRegistry.register('eventStreamService', eventStreamService);
+const cors = require('cors');
+const connectDB = require('./serverSetup/database');
+require('./services/scheduler');
 
 // Connect to database
 connectDB();
@@ -25,20 +25,14 @@ app.use(express.urlencoded({ extended: false }));
 })();
 
 // API Routes
-app.use("/api/auth", require("./api/routes/userModule/auth.routes"));
-app.use(
-  "/api/eventstream",
-  require("./api/routes/eventStream/eventStream.routes.js")
-);
-app.use("/api/weather", require("./api/routes/weather/weather.routes"));
-app.use(
-  "/api/locations",
-  require("./api/routes/locationModule/location.routes.js")
-);
-app.use("/api/crops", require("./api/routes/cropModule/crop.routes.js"));
+app.use('/api/auth', require('./api/routes/userModule/auth.routes'));
+app.use('/api/eventstream', require('./api/routes/eventStream/eventStream.routes.js'));
+app.use('/api/weather', require('./api/routes/weather/weather.routes'));
+app.use('/api/locations', require('./api/routes/locationModule/location.routes.js'));
+app.use('/api/crops', require('./api/routes/cropModule/crop.routes.js'));
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get('/', (req, res) => {
+    res.send('API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
